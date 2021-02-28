02e5c9222ebbe78a383066b8e7279d85817ace55
Angela Flowers <me@angela.flowers>
2021-02-28 14:57:18 -0500

    added backslash feature

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
           output += char.toLowerCase();
         }
         caps = !caps;
+      } else if ((char === "\\" && i > 0) && formText.charAt(i - 1) !== "\\") {
+        caps = !caps
       } else {
         output += char;
       }      
@@ -43,8 +45,24 @@ function App() {
 
   return (
     <div className="App">
+      <h2>{firstUpper ? "MoCkInG tExT gEnErAtOr:" : "mOcKiNg TeXt GeNeRaToR:"}</h2>
+      <div className="instructions">
+        <h3>Instructions:</h3>
+
+        <ul>
+          <li>Enter your text in the top box.</li>
+          <li>The mememified text will automatically appear in the lower box.</li>
+          <li>Use the "invert case" button to switch all uppercase and lowercase letters in the output.</li>
+          <li>If the text is not to your liking in either configuration, you can edit the case of individual letters by placing a backslash ("\") in front of them.</li>
+          <ul>
+            <li>The backslash indicates that a given character should be the same case as the character before it.</li>
+            <li>The backslash may not modify the first character of your text.</li>
+            
+          </ul>
+        </ul>
+      </div>
       <form className="text-memer">
-        <h2>{firstUpper ? "MoCkInG tExT gEnErAtOr:" : "mOcKiNg TeXt GeNeRaToR:"}</h2>
+        
         <label htmlFor="input">Your text:</label>
           
           <input
