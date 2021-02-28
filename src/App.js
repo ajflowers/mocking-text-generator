c3829014a5ce206aa44d7540d8f4794864bf0562
Angela Flowers <me@angela.flowers>
2021-02-27 23:24:05 -0500

    input field, character logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -6,9 +6,43 @@ function App() {
   const [newText, setNewText] = useState('');
   const [firstUpper, setFirstUpper] = useState(false);
 
+  useEffect(() => {
+    var caps = firstUpper;
+    var output = ""
+    for(var i = 0; i < formText.length; i++) {
+      var char = formText.charAt(i);
+      if (char.match(/[a-z]/i)) {
+        if (caps) {
+          output += char.toUpperCase();
+        } else {
+          output += char.toLowerCase();
+        }
+        caps = !caps;
+      } else {
+        output += char;
+      }      
+    }
+    setNewText(output);
+    console.log(newText)
+  });
+
+  const handleChange = event => {
+    event.preventDefault();
+    setFormText(event.target.value);
+    console.log(event.target.value);
+
+  }
+
+
+
   return (
     <div className="App">
-      
+      <h2>EnTeR yOuR tExT hErE:</h2>
+      <input
+        type="text"
+        name="input"
+        onChange={event => handleChange(event)}
+      />
      
     </div>
   );
